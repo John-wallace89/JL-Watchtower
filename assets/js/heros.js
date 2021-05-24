c9bc49f93e5261a914b72ccf953102e56c81a49a
John-wallace89 <john.wallace@heineken.co.uk>
2021-05-24 10:50:30 +0000

    Update: added map and Js for animated icon and moved card flip divs to recruit section.

--- assets/js/heros.js
+++ assets/js/heros.js
@@ -1,14 +1,108 @@
-//Credit - took influences from https://www.ordinarycoders.com/blog/article/codepen-bootstrap-card-hovers//
-//and https://codepen.io/jacoboakley/pen/ZpRbqB
+// Credit--- Mapbox https://docs.mapbox.com/mapbox-gl-js/example/add-image-animated/--
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obi13YWxsYWNlODkiLCJhIjoiY2tvdHNuaDNpMGVmejJvcWpuZGUzdmJmbiJ9.BOt8BK_I-TyL3DxXUeKPig';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/john-wallace89/ckoxcyrn00j2w17mqtb9xelyf',
+center: [-74.5, 40],
+zoom: 1, // starting zoom
+});
 
-function flip(event) {
-    var element = event.currentTarget;
-    if (element.className === "card card-flip") {
-        if (element.style.transform == "rotateY(180deg)") {
-            element.style.transform = "rotateY(0deg)";
-            document.getElementsByClassName("front").style = "display: none";
-        } else {
-            element.style.transform = "rotateY(180deg)";
-        }
-    }
+map.addControl(new mapboxgl.NavigationControl());
+
+ 
+var size = 200;
+ 
+// implementation of CustomLayerInterface to draw a pulsing dot icon on the map
+var pulsingDot = {
+width: size,
+height: size,
+data: new Uint8Array(size * size * 4),
+ 
+// get rendering context for the map canvas when layer is added to the map
+onAdd: function () {
+var canvas = document.createElement('canvas');
+canvas.width = this.width;
+canvas.height = this.height;
+this.context = canvas.getContext('2d');
+},
+ 
+// called once before every frame where the icon will be used
+render: function () {
+var duration = 1000;
+var t = (performance.now() % duration) / duration;
+ 
+var radius = (size / 2) * 0.3;
+var outerRadius = (size / 2) * 0.7 * t + radius;
+var context = this.context;
+ 
+// draw outer circle
+context.clearRect(0, 0, this.width, this.height);
+context.beginPath();
+context.arc(
+this.width / 2,
+this.height / 2,
+outerRadius,
+0,
+Math.PI * 2
+);
+context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+context.fill();
+ 
+// draw inner circle
+context.beginPath();
+context.arc(
+this.width / 2,
+this.height / 2,
+radius,
+0,
+Math.PI * 2
+);
+context.fillStyle = 'rgba(255, 100, 100, 1)';
+context.strokeStyle = 'white';
+context.lineWidth = 2 + 4 * (1 - t);
+context.fill();
+context.stroke();
+ 
+// update this image's data with data from the canvas
+this.data = context.getImageData(
+0,
+0,
+this.width,
+this.height
+).data;
+ 
+// continuously repaint the map, resulting in the smooth animation of the dot
+map.triggerRepaint();
+ 
+// return `true` to let the map know that the image was updated
+return true;
+}
 };
+ 
+map.on('load', function () {
+map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+ 
+map.addSource('points', {
+'type': 'geojson',
+'data': {
+'type': 'FeatureCollection',
+'features': [
+{
+'type': 'Feature',
+'geometry': {
+'type': 'Point',
+'coordinates': [-74, 50],
+}
+}
+]
+}
+});
+map.addLayer({
+'id': 'points',
+'type': 'symbol',
+'source': 'points',
+'layout': {
+'icon-image': 'pulsing-dot'
+}
+});
+});
