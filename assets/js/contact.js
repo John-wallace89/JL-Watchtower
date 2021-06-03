0514034a21bba25f2a6b732f3f0040b379adae2f
John-wallace89 <john.wallace@heineken.co.uk>
2021-06-03 20:13:57 +0000

    Update:Added contact form and contact.js to 'contact us' section.

---
+++ assets/js/contact.js
@@ -0,0 +1,17 @@
+function contactForm(contactForm) {
+    emailjs.send("service_38n4p0g","template_watchtower", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "area": contactForm.area.value,
+        "email_body": contactForm.emailbody.value,
+    })
+        .then(
+             function(response) {
+            console.log("Email sent!", response);
+        },
+        function(error) {
+            console.log("Error", error);
+        }
+    );
+    return false;
+}
