15d588292070ccf5d9de2c39afe4d63fd4aec56a
John-wallace89 <john.wallace@heineken.co.uk>
2021-06-04 19:00:52 +0000

    Update: Added flyto zoom and markers to hero map, added footer to scanner.html.

--- assets/js/heros.js
+++ assets/js/heros.js
@@ -4,111 +4,14 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/john-wallace89/ckoxcyrn00j2w17mqtb9xelyf',
     center: [0, 0],
-    zoom: 1.5, // starting zoom
+    zoom: 1, // starting zoom
 });
 
 map.addControl(new mapboxgl.NavigationControl());
 
-
-var size = 200;
-
-// implementation of CustomLayerInterface to draw a pulsing dot icon on the map
-var pulsingDot = {
-    width: size,
-    height: size,
-    data: new Uint8Array(size * size * 4),
-
-    // get rendering context for the map canvas when layer is added to the map
-    onAdd: function () {
-        var canvas = document.createElement('canvas');
-        canvas.width = this.width;
-        canvas.height = this.height;
-        this.context = canvas.getContext('2d');
-    },
-
-    // called once before every frame where the icon will be used
-    render: function () {
-        var duration = 1000;
-        var t = (performance.now() % duration) / duration;
-
-        var radius = (size / 2) * 0.3;
-        var outerRadius = (size / 2) * 0.7 * t + radius;
-        var context = this.context;
-
-        // draw outer circle
-        context.clearRect(0, 0, this.width, this.height);
-        context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            outerRadius,
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-        context.fill();
-
-        // draw inner circle
-        context.beginPath();
-        context.arc(
-            this.width / 2,
-            this.height / 2,
-            radius,
-            0,
-            Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 100, 100, 1)';
-        context.strokeStyle = 'white';
-        context.lineWidth = 2 + 4 * (1 - t);
-        context.fill();
-        context.stroke();
-
-        // update this image's data with data from the canvas
-        this.data = context.getImageData(
-            0,
-            0,
-            this.width,
-            this.height
-        ).data;
-
-        // continuously repaint the map, resulting in the smooth animation of the dot
-        map.triggerRepaint();
-
-        // return `true` to let the map know that the image was updated
-        return true;
-    }
-};
-
-map.on('load', function () {
-    map.addImage('pulsing-dot', pulsingDot, {
-        pixelRatio: 2
-    });
-
-    map.addSource('points', {
-        'type': 'geojson',
-        'data': {
-            'type': 'FeatureCollection',
-            'features': [{
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': [0, 0],
-                }
-            }]
-        }
-    });
-    map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-            'icon-image': 'pulsing-dot'
-        }
-    });
-});
-
 let heroButton = document.getElementById('hero-btn');
 heroButton.addEventListener('click', heroLocation);
+
 
 function heroLocation() {
 
@@ -121,9 +24,17 @@ function heroLocation() {
         document.getElementById('hero-location').innerHTML =
             (`<h4>Hero - Superman</h4>
             <h4>Location - Fortress of Solitude</h4>
-            <h4>Co-ordinates - 90.00° N, 135.00° W</h4>
+            <h4>Co-ordinates - 76.2506° N, 100.1140° W</h4>
             <h4>Mission - Superman has gone back to the Fortress of solitude to find answers about Krypton, 
             his home world</h4>`);
+        var marker = new mapboxgl.Marker()
+            .setLngLat([-100, 76])
+            .addTo(map); // add the marker to the map
+        map.flyTo({
+            center: [-100, 76],
+            zoom: 3,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
 
     } else if (herosResult === herosArray[1]) {
 
@@ -131,9 +42,17 @@ function heroLocation() {
             (`<img src="assets/images/unsplash_batman.jpg" style="height:200px;width:300px;" >
             <h4>Hero - Batman</h4>
             <h4>Location - Gotham City</h4>
-            <h4>Co-ordinates:40.7128° N, 74.0060° W</h4>
+            <h4>Co-ordinates - 40.7128° N, 74.0060° W</h4>
             <h4>Mission - We've picked up reports from GCPD of explosions at ACE chemicals, 
             We've sent Batman to investigate</h4>`);
+        var marker = new mapboxgl.Marker()
+            .setLngLat([-74, 40])
+            .addTo(map); // add the marker to the map
+        map.flyTo({
+            center: [-74.5, 40],
+            zoom: 3,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
 
     } else if (herosResult === herosArray[2]) {
 
@@ -143,6 +62,14 @@ function heroLocation() {
             <h4>Co-ordinates - 48.8566° N, 2.3522° E</h4>
             <h4>Mission - We've sent Wonder Women to link up with Interpol to stop a 
             potential robbery at The Louvre</h4>`);
+        var marker = new mapboxgl.Marker()
+            .setLngLat([2.3, 48])
+            .addTo(map); // add the marker to the map
+        map.flyTo({
+            center: [2.3, 48],
+            zoom: 3,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
 
     } else if (herosResult === herosArray[3]) {
 
@@ -152,6 +79,14 @@ function heroLocation() {
             <h4>Co-ordinates - 11.3493° N, 142.1996° E</h4>
             <h4>Mission - We picked up a distress call from a deep sea exploration vessel, 
             Aquaman is coming to their aid</h4>`);
+        var marker = new mapboxgl.Marker()
+            .setLngLat([142, 11.3])
+            .addTo(map); // add the marker to the map
+        map.flyTo({
+            center: [142, 11.3],
+            zoom: 3,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
 
     } else if (herosResult === herosArray[4]) {
 
@@ -161,6 +96,14 @@ function heroLocation() {
             <h4>Co-ordinates - 41.8781° N, 87.6298° W</h4>
             <h4>Mission - The Flash is in Central City, 
             following up a lead on a series of thefts in the downtown area.</h4>`);
+        var marker = new mapboxgl.Marker()
+            .setLngLat([-87, 41.8])
+            .addTo(map); // add the marker to the map
+        map.flyTo({
+            center: [-87, 41.8],
+            zoom: 3,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
 
     } else if (herosResult === herosArray[5]) {
 
@@ -170,6 +113,7 @@ function heroLocation() {
             <h4>Hero: Martian Manhunter</h4>
             <h4>Location: Unknown</h4>
             <h4>Not on Earth, <a href="scanner.html">click here</a> to activate long range scanners.</h4>`);
+            document.getElementById('map').classList.add('hide');
 
     } else {
         console.log("no hero found!");
